fa32c35aa8dbec873a0ef11253c0ce15bfa6d512
unknown <mkomljenovic11@gmail.com>
2020-09-27 12:19:19 +0200

    Some random outbursts before I take a nap

--- threed-frontend/src/app/components/model-create/model-create.component.ts
+++ threed-frontend/src/app/components/model-create/model-create.component.ts
@@ -1,4 +1,5 @@
 import { Router } from '@angular/router';
+import {gsap} from 'gsap/all';
 import { ApiService } from './../../service/api.service';
 import { Component, OnInit, NgZone } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from "@angular/forms";
@@ -23,7 +24,18 @@ export class ModelCreateComponent implements OnInit {
     this.mainForm();
   }
 
-  ngOnInit() { }
+ngOnInit(): void {
+  this.createAnimation();
+  }
+
+  createAnimation(){
+    const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+    tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
+    tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
+    tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
+    tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
+    tl.fromTo(".big-text", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
+  }
 
   // Form Scheme
   mainForm() {
--- threed-frontend/src/app/components/model-list/model-list.component.ts
+++ threed-frontend/src/app/components/model-list/model-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from './../../service/api.service';
+import {gsap} from 'gsap/all';
 
 @Component({
   selector: 'app-model-list',
@@ -14,7 +15,18 @@ export class ModelListComponent implements OnInit {
     this.readModel(); 
   } 
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+  this.createAnimation();
+  }
+
+   createAnimation(){
+    const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+    tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
+    tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
+    tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
+    tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
+    tl.fromTo(".big-text", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
+  }
 
   readModel() {
     this.apiService.getModels().subscribe((data) => {
